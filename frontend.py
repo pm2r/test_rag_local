e7ddc6e4660b9a2dab5049fa0af0b022eeca6c65
pm2r <7140251+pm2r@users.noreply.github.com>
2024-11-15 19:48:39 +0100

    fix

--- frontend.py
+++ frontend.py
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 # Configurazione Connessione Backend
 # -----------------------------------------------------------------------------
 # Sostituire con il tuo URL ngrok
-BACKEND_URL = st.secrets.get("BACKEND_URL", "https://af27-34-147-36-231.ngrok-free.app" )
+BACKEND_URL = st.secrets.get("BACKEND_URL", "https://ec28-34-147-36-231.ngrok-free.app" )
 
 # -----------------------------------------------------------------------------
 # Configurazione Pagina e Stile
